ce0a5793b60136ff1a14f4ff8e2f0e823581488a
job314 <job314job@pm.me>
2022-05-19 01:47:54 +0900

    TabView追加
    タブバー追加

--- ios/shuttle-timetable/shuttle-timetable/ContentView.swift
+++ ios/shuttle-timetable/shuttle-timetable/ContentView.swift
@@ -9,11 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            TabHome()
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Home")
+                }
+            TabSetting()
+                .tabItem {
+                    Image(systemName: "gear.circle.fill")
+                    Text("Setting")
+                }
+        }
     }
 }
 
+struct TabHome: View {
+    var body: some View {
+        Text("MakingHomePage...")
+    }
+}
+struct TabSetting: View {
+    var body: some View {
+        Text("Setting")
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
