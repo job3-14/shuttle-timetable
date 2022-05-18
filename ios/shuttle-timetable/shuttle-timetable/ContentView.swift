1717fecda83a8758bbdd3378eba3b85a5187e20c
job314 <job314job@pm.me>
2022-05-19 00:18:52 +0900

    iosファイル作成

---
+++ ios/shuttle-timetable/shuttle-timetable/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  shuttle-timetable
+//
+//  Created by 酒井歩夢 on 2022/05/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios/shuttle-timetable/shuttle-timetable/shuttle_timetableApp.swift
@@ -0,0 +1,17 @@
+//
+//  shuttle_timetableApp.swift
+//  shuttle-timetable
+//
+//  Created by 酒井歩夢 on 2022/05/19.
+//
+
+import SwiftUI
+
+@main
+struct shuttle_timetableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
